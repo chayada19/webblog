6ab60d12a28171eb4f8e69d72c3d876bc38b0513
chayada19 <noyhuahatc@gmail.com>
2023-08-16 01:51:34 +0700

    page 54

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,12 @@ const app = express()
 app.get('/status', function (req,res){
     res.send('Hello nodejs server belong to chayada')
 })
- 
+
+app.get('/hello/:name', function (req,res) {
+    console.log('hello - ' + req.params.person)
+    res.send('sey hello with ' + req.params.person)
+   })
+
 let port = 8081
 app.listen(port, function(){
     console.log('server running on ' + port)
