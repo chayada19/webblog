a4179777b5ae5b9740daa96b0629c606f3828c65
chayada19 <noyhuahatc@gmail.com>
2023-08-16 09:52:14 +0700

    finish chapter4

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,41 @@
 let express = require('express')
+let bodyParser = require('body-parser')
+
 const app = express()
  
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/status', function (req,res){
     res.send('Hello nodejs server belong to chayada')
 })
 
 app.get('/hello/:name', function (req,res) {
-    console.log('hello - ' + req.params.person)
-    res.send('sey hello with ' + req.params.person)
+    console.log('hello - ' + req.params.name)
+    res.send('sey hello with ' + req.params.name)
    })
+
+   app.get('/user/:userId', function (req, res) {
+    res.send('ดูข้อมูลผู้ใช้งาน'+ req.params.userId)
+   })
+
+   app.get('/user/:users', function (req, res) {
+    res.send('ดูข้อมูลผู้ใช้งาน')
+   })
+
+   app.post('/user/', function (req, res) {
+    res.send('สร้างผู้ใช้งาน' + JSON.stringify(req.body))
+   })
+
+   app.put('/user/:userId', function (req, res) {
+    res.send('แก้ไขข้อมูลผู้ใช้' + req.params.userId + ' : ' +
+   JSON.stringify(req.body.name))
+   })
+
+   app.delete('/user/:userId', function (req, res) {
+    res.send('ลบผุ้ใช้งาน' + req.params.userId + ' : ' + 
+    JSON.stringify(req.body.name))
+})
 
 let port = 8081
 app.listen(port, function(){
